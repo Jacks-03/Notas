fc635a147075a3dc6fac44690839d6906e3a7871
Jaqueline Juarez Silva <jaquelinejuarez.silva03@gmail.com>
2022-04-08 15:28:47 -0500

    Dividiendo la interfaz con Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,54 @@
 import { useState } from "react";
 
 const App =() => {
-//hool -> use .....
+  //hool -> use .....
   //stage ->useState
-
   const [inputsState, setInputsState]= useState({
     title:"",
     date:"",
     note:"",
   });
-
-
 const handleInputChange =(event) => { 
  setInputsState({ ...inputsState, [event.target.name]: event.target.value});
 }
+
+
 const handleClickLimpiar = (event) => {
-  setInputsState({ ...setInputsState, title:"", date:"", note:"" });
+  setInputsState({title:"", date:"", note:"" });
 };
 
-
-
-
   return (
-    <div className="App">
-      <h1>Notas</h1>
+    <div className="App container">
+      <div className = "row" > 
+        <div className = "col" >
+          <h3>Lista</h3>
+        </div>
 
-      <label>
-        Titulo
-      <input id="title" name="title" type="text" onChange={handleInputChange} value={inputsState.title} /> 
-      </label>
-      <br/>
-      <label>
+        <div className="col">
+        <h3>Notas</h3>
+        <label className="mb-2">
+          Titulo
+          <input id="title" name="title" type="text" onChange={handleInputChange} value={inputsState.title} /> 
+          </label>
+         <br /> 
+         <label className="mb-2">
         Fecha
       <input id="date" name="date" type="text" onChange={handleInputChange} value={inputsState.date} /> 
       </label>
       <br/>
-        <label>
+      <label>
         Nota
       <input id="note" name="note" type="text" onChange={handleInputChange} value={inputsState.note} /> 
       </label>
-
-      <button className="btn btn-primary me-2" onClick={handleClickLimpiar}>Limpiar</button>
-
+      </div>
     </div>
+    <hr />
+
+    <div className = "row" > 
+        <button className="btn btn-primary me-2" onClick={handleClickLimpiar}>Limpiar</button>
+        </div>
+        </div>
   );
 }
+
 export default App;
